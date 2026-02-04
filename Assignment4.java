5e64972d48e75e8423d0f0e0fdf70cb960a0f371
SpoopyEd <eduardoaguilera1031@gmail.com>
2026-02-04 02:39:17 -0800

    Created user input loop, fixed problems with nextInt and nextLine, as well as infinite catch statement loops

--- Assignment4.java
+++ Assignment4.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Assignment4 {
     public static Scanner input = new Scanner(System.in);  // Create a Scanner object
     public static void main(String[] args) {
+        boolean correctIn = false; // initalizing correct input, keeps a loop going until user gives correct input
         String username; // initializing username
         int dob = 0; // initialzing date of birth
         String address; // initialzing adress
@@ -14,23 +15,29 @@ public class Assignment4 {
         System.out.println("Welcome to Account Creation, Please type out your preffered Username");
         username = input.nextLine();
         System.out.println("Please type out your date of birth in digit form MMDDYYYY");
-        try {
-            dob = input.nextInt();
-        } catch (Exception e) {
-            System.out.println("Date of birth must be in digit form MMDDYYYY, no letters or symbols allowed");
-        } finally {
-            input.nextLine(); // discards new line, avoids skipping next line
-        }
-        // likely loop until correct format given
+        while (correctIn == false) {
+            try {
+                dob = input.nextInt();
+                correctIn = true;
+            } catch (Exception e) {
+                System.out.println("Date of birth must be in digit form MMDDYYYY, no letters or symbols allowed");
+            } finally {
+                input.nextLine(); // clears input so that there are no issues when calling nextLine() or infinite catch loop
+            }
+        } correctIn = false; // resets loop for next prompt
         System.out.println("Please type out your address");
         address = input.nextLine();
         System.out.println("Please type out your phone number");
-        try {
-            phoneNumber = input.nextInt();
-        } catch (Exception e) {
-            System.out.println("Phone number must be in digit form, no letters or symbols allowed");
+        while (correctIn == false) {
+            try {
+                phoneNumber = input.nextInt();
+                correctIn = true;
+            } catch (Exception e) {
+                System.out.println("Phone number must be in digit form, no letters or symbols allowed");
+            } finally {
+                input.nextLine(); // clears input so that there are no issues when calling nextLine() or infinite catch loop
+            }
         }
-        // likely loop until correct format given
         Account account = new Account(username, dob, address, phoneNumber);
         System.out.printf("Account Created %n Username: %s,%n Date of Birth: %d,%n Address: %s,%n Phone Number: %d", account.getUsername(), account.getDob(), account.getAddress(), account.getPhoneNumber()); // print account information
     }
