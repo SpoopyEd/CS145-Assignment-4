c63bc91b307fdad4eb15703f9ef88271880c23d6
SpoopyEd <eduardoaguilera1031@gmail.com>
2026-02-03 22:12:23 -0800

    Variables required initializing because of error when created in try catch statement

--- Assignment4.java
+++ Assignment4.java
@@ -7,20 +7,24 @@ import java.util.Scanner;
 public class Assignment4 {
     public static Scanner input = new Scanner(System.in);  // Create a Scanner object
     public static void main(String[] args) {
+        String username; // initializing username
+        int dob = 0; // initialzing date of birth
+        String address; // initialzing adress
+        int phoneNumber = 0; // initializing phone number
         System.out.println("Welcome to Account Creation, Please type out your preffered Username");
-        String username = input.nextLine();
+        username = input.nextLine();
         System.out.println("Please type out your date of birth in digit form MMDDYYYY");
         try {
-            int dob = input.nextInt();
+            dob = input.nextInt();
         } catch (Exception e) {
             System.out.println("Date of birth must be in digit form MMDDYYYY, no letters or symbols allowed");
         }
         // likely loop until correct format given
         System.out.println("Please type out your adress");
-        String address = input.nextLine();
+        address = input.nextLine();
         System.out.println("Please type out your phone number");
         try {
-            int phoneNumber = input.nextInt();
+            phoneNumber = input.nextInt();
         } catch (Exception e) {
             System.out.println("Phone number must be in digit form, no letters or symbols allowed");
         }
