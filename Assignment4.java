37966c41eec7af8f9b678a348d691eac02092938
SpoopyEd <eduardoaguilera1031@gmail.com>
2026-02-03 21:59:11 -0800

    Built main, added comments for intentions

--- Assignment4.java
+++ Assignment4.java
@@ -8,5 +8,16 @@ public class Assignment4 {
     public static Scanner input = new Scanner(System.in);  // Create a Scanner object
     public static void main(String[] args) {
         System.out.println("Welcome to Account Creation, Please type out your preffered Username");
+        String username = input.nextLine();
+        System.out.println("Please type out your date of birth in digit form MMDDYYYY");
+        int dob = input.nextInt();
+        // try catch statement
+        System.out.println("Please type out your adress");
+        String address = input.nextLine();
+        System.out.println("Please type out your phone number");
+        int phoneNumber = input.nextInt();
+        // try catch statement
+        Account account = new Account(username, dob, address, phoneNumber);
+        System.out.printf(""); // print account information
     }
 }
